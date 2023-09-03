a3cfbd49e7ec349704c47fc7827ce0e79e0f9655
Yej1n <hraniee@naver.com>
2023-09-03 23:56:29 +0900

    feat : 상세 설명, 아이콘 추가

--- src/components/Section/Footer/style.ts
+++ src/components/Section/Footer/style.ts
@@ -1,1 +1,84 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+import { font } from "styles/font";
+import color from "styles/color";
+
+export const FooterLayout = styled.div`
+    width: 100%;
+    height: 400px;
+    color: ${color.white};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: ${color.black};
+`;
+
+export const FooterContainer = styled.div`
+    width: 76%;
+    height: 60%;
+`;
+
+export const FooterInfoBox = styled.footer`
+    display: flex;
+    flex-direction: column;
+
+`;
+
+export const FooterTitle = styled.span`
+    ${font.H2};
+`;
+
+export const FooterHGroup = styled.hgroup`
+    padding-bottom: 2%;
+    margin-bottom: 2%;
+    display: flex;
+    flex-direction: column;
+`;
+
+export const FooterSection = styled.section`
+    display: flex;
+    flex-direction: column;
+    gap: 2px;
+    border-bottom: 1px solid ${color.white};
+    width: 25%;
+    padding-bottom: 3%;
+    justify-content: center;
+    height: 30%;
+    margin: 0;
+`;
+
+export const FooterSubTitle = styled.span`
+    ${font.H5};
+    margin-bottom: 3%;
+`;
+
+export const FooterPolicyText = styled.span`
+    ${font.p4};
+    color: ${color.white};
+`;
+
+export const FooterCopyright = styled.span`
+    margin-top: 3%;
+    ${font.p4};
+    color: ${color.white};
+`;
+
+export const Icon = styled.img`
+    width: 1rem;
+`;
+
+export const Detaile = styled.section`
+    display: flex;
+    flex-direction: column;
+    gap: 2px;
+    width: 60%;
+    justify-content: center;
+    margin-left: 30%;
+    height: 30%;
+`
+
+export const Image = styled.section`
+    display: flex;;
+    margin-left: 60%;
+    gap: 2px;
+    height: 30%;
+`
