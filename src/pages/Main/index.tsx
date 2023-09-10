2cd818e41abd4cbaf27e45738b271df247932897
jyh071116 <jyh071116@gmail.com>
2023-09-10 20:24:19 +0900

    chore: 코드 스타일 수정

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,9 +1,11 @@
 import Header from "components/Section/Header";
+import Footer from "components/Section/Footer";
 
 const Main = () => {
   return (
     <>
-      <Header showAccount={true} />
+      <Header showAccount />
+      <Footer />
     </>
   );
 };
